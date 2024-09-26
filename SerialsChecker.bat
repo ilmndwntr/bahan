d891dec4b9d98f0a730a05aea23e3665717f3a91
7xik <136289530+7xik@users.noreply.github.com>
2024-09-26 01:17:27 +0100

    Update SerialsChecker.bat

--- SerialsChecker.bat
+++ SerialsChecker.bat
@@ -1,8 +1,5 @@
 @echo off
 cls
-echo [90m==========================[97m
-echo Bios
-wmic bios get serialnumber
 wmic csproduct get uuid
 echo CPU
 wmic cpu get serialnumber
@@ -13,5 +10,4 @@ echo Baseboard
 wmic baseboard get serialnumber
 wmic baseboard get manufacturer
 wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress
-echo [90m==========================[97m
 pause >nul
